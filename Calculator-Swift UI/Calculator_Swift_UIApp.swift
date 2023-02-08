818886097f8dd8dd00c8bb4ce54fb979e62d8153
Qeti Avanashvili <qetiavanashvili@gmail.com>
2023-02-09 00:19:24 +0400

    Initial Commit

---
+++ Calculator-Swift UI/Calculator_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_Swift_UIApp.swift
+//  Calculator-Swift UI
+//
+//  Created by Qeti Avanashvili on 09.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
